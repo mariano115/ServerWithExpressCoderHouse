682fdfae4fafac7995ad122751b33af50aad5619
Mariano <marianogermanguerrero@gmail.com>
2022-09-07 11:23:13 -0300

    port actulized

--- server.js
+++ server.js
@@ -2,22 +2,27 @@ const express = require("express");
 const { Contenedor } = require("./Contenedor/Contenedor");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 let instanciaContenedor;
 
+/* Creating a server and listening on port 8080. */
 const server = app.listen(PORT, () => {
   console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
   instanciaContenedor = new Contenedor("productos");
 });
 
+/* Listening for errors on the server. */
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
 
+/* A route that returns all the products from the database. */
 app.get("/productos", async (req, res) => {
   res.send(await instanciaContenedor.getAll());
 });
 
+/* A route that returns a random product from the database. */
 app.get("/productoRandom", async (req, res) => {
-  let productos = await instanciaContenedor.getAll();
-  let productoRandom = productos[Math.floor(Math.random() * productos.length)];
+  const productos = await instanciaContenedor.getAll();
+  const productoRandom =
+    productos[Math.floor(Math.random() * productos.length)];
   res.send(productoRandom);
 });
